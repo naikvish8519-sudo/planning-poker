1c6b9eb9c23dac04dc77517eae3db4be32e85a26
Vishwanath <vishwasjce@gmail.com>
2026-07-14 19:22:48 -0400

    theme changed

--- src/app/components/poker-cards/poker-cards.ts
+++ src/app/components/poker-cards/poker-cards.ts
@@ -1,21 +1,48 @@
+// import { Component, EventEmitter, Output } from '@angular/core';
+
+// @Component({
+//   selector: 'app-poker-cards',
+//   standalone: false,
+//   templateUrl: './poker-cards.html',
+//   styleUrl: './poker-cards.scss',
+// })
+// export class PokerCards {
+
+//   cards = [1, 2, 3, 5, 8, 13, 21];
+
+//   selectedCard: number | null = null;
+
+//   @Output()
+//   voteSelected = new EventEmitter<number>();
+
+//   selectCard(card: number) {
+
+//     this.selectedCard = card;
+
+//     this.voteSelected.emit(card);
+
+//   }
+
+// }
+
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-poker-cards',
   standalone: false,
   templateUrl: './poker-cards.html',
-  styleUrl: './poker-cards.scss',
+  styleUrl: './poker-cards.scss'
 })
 export class PokerCards {
+
+  @Output()
+  voteSelected = new EventEmitter<number>();
 
   cards = [1, 2, 3, 5, 8, 13, 21];
 
   selectedCard: number | null = null;
 
-  @Output()
-  voteSelected = new EventEmitter<number>();
-
-  selectCard(card: number) {
+  selectCard(card: number): void {
 
     this.selectedCard = card;
 
